3529393c906a3304c6fae0100aaf4402918e6f7e
josiasjb <josiasjb05@gmail.com>
2021-10-14 11:40:22 -0300

    Exponde API´s Rest para consultar clientes no

---
+++ src/main/java/com/gvendas/gestaovendas/servive/ClienteServico.java
@@ -0,0 +1,27 @@
+package com.gvendas.gestaovendas.servive;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gvendas.gestaovendas.entidades.Cliente;
+import com.gvendas.gestaovendas.repositorio.ClienteRepositorio;
+
+@Service
+public class ClienteServico {
+
+	@Autowired
+	private ClienteRepositorio clienteRepositorio;
+	
+	public List<Cliente> listarTodas() {
+		return clienteRepositorio.findAll();
+		
+	}
+
+	public Optional<Cliente> buscarPorCodigo(Long codigo) {
+		return clienteRepositorio.findById(codigo);
+		
+	}
+}
