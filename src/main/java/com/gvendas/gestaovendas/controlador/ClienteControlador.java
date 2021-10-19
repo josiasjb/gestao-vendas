6cc6665295f33030825ced5bd921ee13fd04ea88
josiasjb <josiasjb05@gmail.com>
2021-10-19 14:31:57 -0300

    Validando cliente com Bean Validation/Atualizando cliente/Deletando Cliente no banco de dados

--- src/main/java/com/gvendas/gestaovendas/controlador/ClienteControlador.java
+++ src/main/java/com/gvendas/gestaovendas/controlador/ClienteControlador.java
@@ -4,16 +4,23 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gvendas.gestaovendas.dto.categoria.CategoriaResponseDTO;
+import com.gvendas.gestaovendas.dto.cliente.ClienteRequestDTO;
 import com.gvendas.gestaovendas.dto.cliente.ClienteResponseDTO;
-import com.gvendas.gestaovendas.entidades.Categoria;
 import com.gvendas.gestaovendas.entidades.Cliente;
 import com.gvendas.gestaovendas.servive.ClienteServico;
 
@@ -42,5 +49,24 @@ public class ClienteControlador {
 		return cliente.isPresent() ? ResponseEntity.ok(ClienteResponseDTO.converterParaClienteDTO(cliente.get()))
 				: ResponseEntity.notFound().build();
 	}
-
+	
+	@ApiOperation(value = "Salvar", nickname = "salvarCliente")
+	@PostMapping
+	public ResponseEntity<ClienteResponseDTO> salvar(@Valid @RequestBody ClienteRequestDTO clienteDto ) {
+		Cliente clienteSalvo = clienteServico.salvar(clienteDto.converteParaEntidade());
+		return ResponseEntity.status(HttpStatus.CREATED).body(ClienteResponseDTO.converterParaClienteDTO(clienteSalvo));
+	}
+	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<ClienteResponseDTO> atualizar(@PathVariable long codigo, @Valid @RequestBody ClienteRequestDTO clienteDto) {
+		Cliente clienteAtualizado = clienteServico.atualizar(codigo, clienteDto.converteParaEntidade(codigo));
+		return ResponseEntity.ok(ClienteResponseDTO.converterParaClienteDTO(clienteAtualizado));
+	}
+	
+	@ApiOperation(value = "Deletar", nickname = "deleteCliente")
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Long codigo) {
+		clienteServico.deletar(codigo);
+	}
 }
