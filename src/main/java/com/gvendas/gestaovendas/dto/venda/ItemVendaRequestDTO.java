966a725cf34b04fb56b4e0a88532120bd49614db
josiasjb <josiasjb05@gmail.com>
2021-10-22 10:25:30 -0300

    Criando objeto Venda Request DTO

---
+++ src/main/java/com/gvendas/gestaovendas/dto/venda/ItemVendaRequestDTO.java
@@ -0,0 +1,44 @@
+package com.gvendas.gestaovendas.dto.venda;
+
+import java.math.BigDecimal;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel("Itens da venda requisição DTO")
+public class ItemVendaRequestDTO {
+
+	@ApiModelProperty(value = "Código produto")
+	private Long codigoProduto;
+
+	@ApiModelProperty(value = "Quantidade")
+	private Integer quantidade;
+
+	@ApiModelProperty(value = "Preço vendido")
+	private BigDecimal precoVenda;
+
+	public Long getCodigoProduto() {
+		return codigoProduto;
+	}
+
+	public void setCodigoProduto(Long codigoProduto) {
+		this.codigoProduto = codigoProduto;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getPrecoVenda() {
+		return precoVenda;
+	}
+
+	public void setPrecoVenda(BigDecimal precoVenda) {
+		this.precoVenda = precoVenda;
+	}
+
+}
