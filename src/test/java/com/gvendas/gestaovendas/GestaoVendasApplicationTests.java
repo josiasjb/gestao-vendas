e938254198e2edc02c5930e03946b428a7764f76
josiasjb <josiasjb05@gmail.com>
2021-11-02 20:22:27 -0300

    Iniciando a aplicação pelo docker-compose. Parte 1/2 Iniciando a aplicação pelo docker-compose. Parte 2/3 Iniciando a aplicação pelo docker-compose. Parte 3/3

--- src/test/java/com/gvendas/gestaovendas/GestaoVendasApplicationTests.java
+++ src/test/java/com/gvendas/gestaovendas/GestaoVendasApplicationTests.java
@@ -1,13 +1,12 @@
 package com.gvendas.gestaovendas;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GestaoVendasApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
